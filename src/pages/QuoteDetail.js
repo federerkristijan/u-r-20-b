988444a9c0bc80797c4077097dc61f4af446aa46
federerkristijan <federer.kristijan@gmail.com>
2023-01-03 22:47:38 +0100

    link added to the nested route of quotId

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,27 +1,34 @@
-import React from 'react'
-import { useParams, Route } from 'react-router-dom'
+import React from "react";
+import { useParams, Route, Link } from "react-router-dom";
 
 import Comments from "../components/comments/Comments";
-import HighlightedQuote from '../components/quotes/HighlightedQuote';
-import { Home_Quotes } from './AllQuotes';
+import HighlightedQuote from "../components/quotes/HighlightedQuote";
+import { Home_Quotes } from "./AllQuotes";
 
 const QuoteDetail = () => {
   const params = useParams();
 
-  const quote = Home_Quotes.find(quote => quote.id === params.quoteId);
+  const quote = Home_Quotes.find((quote) => quote.id === params.quoteId);
 
   if (!quote) {
-    return <p>No quote found!</p>
+    return <p>No quote found!</p>;
   }
 
   return (
     <div>
-      <HighlightedQuote text={quote.text} author={quote.author}/>
-      <Route path={`/quotes/${params.quoteId}/comments`} >
+      <HighlightedQuote text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${params.quoteId}`} exact>
+        <div className="centered">
+          <Link className="btn--flat" to={`/quotes/${params.quoteId}/comments`}>
+            Load Comments
+          </Link>
+        </div>
+      </Route>
+      <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
     </div>
-  )
-}
+  );
+};
 
-export default QuoteDetail
+export default QuoteDetail;

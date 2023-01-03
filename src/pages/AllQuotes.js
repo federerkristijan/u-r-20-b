f722251fc405a9f56bf5230272aefd1fec84cf21
federerkristijan <federer.kristijan@gmail.com>
2023-01-03 17:58:23 +0100

    dummy database added

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,9 +1,30 @@
-import React from 'react'
+import React from "react";
+
+const quotes = [
+  {
+    id: "q1",
+    author: "Kiki",
+    text: "Learning React could be fun",
+  },
+  {
+    id: "q2",
+    author: "Kuku",
+    text: "Hoomans are again suck with their machines",
+  },
+  {
+    id: "q3",
+    author: "Kuku",
+    text: "I'm bored...I hate my life!",
+  },
+  {
+    id: "q4",
+    author: "Hadas",
+    text: "I'm tired...can I nap now?",
+  },
+];
 
 const AllQuotes = () => {
-  return (
-    <h1>All Qoutes</h1>
-  )
-}
+  return <h1>All Qoutes</h1>;
+};
 
-export default AllQuotes
+export default AllQuotes;

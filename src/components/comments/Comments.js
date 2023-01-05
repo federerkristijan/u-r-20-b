3ae2161f33ac3d306124cecc021e0c5bc248b736
federerkristijan <federer.kristijan@gmail.com>
2023-01-05 22:00:23 +0100

    useHttp hook & getAllComments()

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,16 +1,23 @@
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import useHttp from '../../hooks/use-http';
+import { getAllComments } from '../../lib/api';
 import classes from './Comments.module.css';
 import NewCommentForm from './NewCommentForm';
-import { useParams } from 'react-router-dom';
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
   const params = useParams();
+  const { sendRequest, status, error } = useHttp(getAllComments);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
+
+  const addedCommentHanlder = () => {
+
+  }
 
   return (
     <section className={classes.comments}>
@@ -20,7 +27,7 @@ const Comments = () => {
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm quoteId={params.quoteId} onAddedComment={} />}
+      {isAddingComment && <NewCommentForm quoteId={params.quoteId} onAddedComment={addedCommentHanlder} />}
       <p>Comments...</p>
     </section>
   );

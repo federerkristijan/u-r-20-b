8876b766bd5a695442a7a0e4df062e1b4c6314f3
federerkristijan <federer.kristijan@gmail.com>
2023-01-05 22:06:44 +0100

    addedCommentHandler

--- src/components/comments/Comments.js
+++ src/components/comments/Comments.js
@@ -1,33 +1,58 @@
-import { useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
 
-import useHttp from '../../hooks/use-http';
-import { getAllComments } from '../../lib/api';
-import classes from './Comments.module.css';
-import NewCommentForm from './NewCommentForm';
+import useHttp from "../../hooks/use-http";
+import { getAllComments } from "../../lib/api";
+import classes from "./Comments.module.css";
+import NewCommentForm from "./NewCommentForm";
+import LoadingSpinner from "../UI/LoadingSpinner";
 
 const Comments = () => {
   const [isAddingComment, setIsAddingComment] = useState(false);
   const params = useParams();
+
+  const { quoteId } = params;
+
   const { sendRequest, status, data: loadedComments } = useHttp(getAllComments);
+
+  useEffect(() => {
+    sendRequest(quoteId);
+  }, [quoteId, sendRequest]);
 
   const startAddCommentHandler = () => {
     setIsAddingComment(true);
   };
 
   const addedCommentHanlder = () => {
+    let comments;
 
+    if (status === "pending") {
+      comments = (
+        <div className="centered">
+          <LoadingSpinner />
+        </div>
+      );
+    }
+  };
+
+  if (status === "completed") {
+    
   }
 
   return (
     <section className={classes.comments}>
       <h2>User Comments</h2>
       {!isAddingComment && (
-        <button className='btn' onClick={startAddCommentHandler}>
+        <button className="btn" onClick={startAddCommentHandler}>
           Add a Comment
         </button>
       )}
-      {isAddingComment && <NewCommentForm quoteId={params.quoteId} onAddedComment={addedCommentHanlder} />}
+      {isAddingComment && (
+        <NewCommentForm
+          quoteId={params.quoteId}
+          onAddedComment={addedCommentHanlder}
+        />
+      )}
       <p>Comments...</p>
     </section>
   );
